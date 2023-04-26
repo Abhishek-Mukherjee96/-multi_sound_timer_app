ddf2bb8b34d7c0daf2dc4830405f5270385b733f
Abhishek Mukherjee <65953490+Abhishek-Mukherjee96@users.noreply.github.com>
2023-04-26 13:21:24 +0530

    modify in sound edit blade

--- resources/views/admin/sound/edit.blade.php
+++ resources/views/admin/sound/edit.blade.php
@@ -34,17 +34,29 @@
                                 <form action="{{route('edit_sound_action',$edit_sound->id)}}" enctype="multipart/form-data" method="post">
                                     @csrf
                                     <div class="row">
-                                        <div class="col-md-6 mb-3">
+                                        <div class="col-md-4 mb-3">
                                             <label><strong>Sound Name</strong> <span class="required_star">*</span></label>
                                             <input class="form-control mb-4" value="{{$edit_sound->sound_name}}" type="text" name="sound_name">
                                             @error('sound_name')
                                             <span class="text text-danger">{{$message}}</span>
                                             @enderror
                                         </div>
-                                        <div class="col-md-6 mb-3">
+                                        <div class="col-md-4 mb-3">
+                                            <label><strong>Category</strong> <span class="required_star">*</span></label>
+                                            <select class="form-control" name="cat_id">
+                                                <option value="" disabled selected>Select One</option>
+                                                @foreach($get_sound_cat as $list)
+                                                <option value="{{$list->id}}" {{ $list->id == $edit_sound->cat_id ? 'selected' : '' }}>{{$list->name}}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('cat_id')
+                                            <span class="text text-danger">{{$message}}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="col-md-4 mb-3">
                                             <label><strong>Upload Sound</strong> <span class="required_star">*</span></label>
                                             <input class="form-control mb-4" type="file" name="file">
-                                            {{$edit_sound->file}}
+                                            {{asset('public/admin/assets/sound-list/'.$edit_sound->file)}}
                                         </div>
                                     </div>
                                     <div class="row">
